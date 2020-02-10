67148a7ea210d26d29790bc2bac782fa7ec07bdc
sebas119 <lopez.sebastian@correunivalle.edu.co>
2020-02-10 21:10:55 +0000

    Create zombie processes to see its behavior

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,1 +1,48 @@
-#!/usr/bin/env bash
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int infinite_while(void);
+
+/**
+ * main - Create 5 zombie processes
+ *
+ * Return: 0 integer
+ */
+int main(void)
+{
+	pid_t child_pid;
+	int c = 5;
+
+	while (c--)
+	{
+		child_pid = fork();
+		if (child_pid > 0)
+		{
+			printf("Zombie process created, PID: %ld\n", (long)child_pid);
+			sleep(0.5);
+		}
+		else
+		{
+			exit(0);
+		}
+	}
+	infinite_while();
+
+	return (0);
+}
+
+/**
+ * infinite_while - Infinite while loop
+ *
+ * Return: 0 Integer.
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
