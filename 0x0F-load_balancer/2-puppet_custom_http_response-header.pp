70a54f57b3149315f38de80defd7bf1e5ad2edb1
Sebastian Lopez H <lopez.sebastian@correounivalle.edu.co>
2020-04-05 03:49:58 +0000

    Puppet config to add header

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -0,0 +1,23 @@
+# Config Nginx and added a header
+exec { 'update_ppa':
+  command  => 'sudo apt-get update',
+  provider => shell,
+}
+
+package { 'nginx':
+  ensure  => installed,
+  require => Exec['update_ppa'],
+}
+
+file_line { 'customHeader':
+  ensure  => present,
+  path    => '/etc/nginx/sites-available/default',
+  after   => ':80 default_server;',
+  line    => "add_header X-Served-By ${HOSTNAME};",
+  require => Package['nginx'],
+}
+
+service { 'nginx server':
+  ensure  => running,
+  require => File_line['customHeader'],
+}
