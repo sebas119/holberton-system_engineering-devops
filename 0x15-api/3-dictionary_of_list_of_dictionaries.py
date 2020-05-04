75e6d3285b3ad1b9598108271c5eed1f45bfce8a
Sebastian Lopez H <lopez.sebastian@correounivalle.edu.co>
2020-05-04 19:21:12 +0000

    Export to JSON all users related to all todos

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,1 +1,34 @@
 #!/usr/bin/python3
+
+"""Export to JSON data of all todos and all users"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+    import json
+
+    base_url = "https://jsonplaceholder.typicode.com"
+
+    req_todos = requests.get("{}/todos".format(base_url))
+    req_users = requests.get("{}/users".format(base_url))
+
+    data_todos = req_todos.json()
+    data_users = req_users.json()
+
+    data = {}
+    for user in data_users:
+        user_id = user.get("id")
+        username = user.get("username")
+        task = []
+        for todo in data_todos:
+            if user_id == todo.get("userId"):
+                temp_dict = {"task": "", "completed": None,
+                             "username": username}
+                completed = todo.get("completed")
+                title = todo.get("title")
+                temp_dict.update({"task": title, "completed": completed})
+                task.append(temp_dict)
+        data.update({user_id: task})
+
+    with open("todo_all_employees.js", mode='wt', encoding='utf-8') as f:
+        json.dump(data, f)
