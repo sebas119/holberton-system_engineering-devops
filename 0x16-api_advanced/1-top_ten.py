933778a34f4bdd39b83ce3bac8e8c3ba6c250d2f
Sebastian Lopez H <lopez.sebastian@correounivalle.edu.co>
2020-05-07 18:11:18 +0000

    prints the titles of the first 10 hot posts listed for a given subreddit.

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+
+"""Reddit Api calls module"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """prints the titles of the first 10 hot posts listed
+    for a given subreddit."""
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'python3:holberton.task:v1.0 (by /u/sebas119_)'}
+    payload = {'limit': '10'}
+    response = requests.get(url, headers=headers,
+                            params=payload, allow_redirects=False)
+
+    if response.status_code == 200:
+        for post in response.json().get('data').get('children'):
+            print(post.get('data').get('title'))
+    else:
+        print("None")
