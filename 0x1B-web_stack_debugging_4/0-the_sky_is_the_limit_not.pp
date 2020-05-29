a5d73f9682075814194755bc8055dd57369c20b6
Sebastian Lopez H <lopez.sebastian@correounivalle.edu.co>
2020-05-29 01:31:07 +0000

    Puppet sky is the limit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,1 +1,10 @@
-#!/usr/bin/env bash
+exec { 'change':
+  command  => 'sed -i "s/15/4096/g" /etc/default/nginx',
+  provider => 'shell',
+}
+
+exec { 'restart':
+  command  => 'sudo service nginx restart',
+  provider => 'shell',
+  require  => Exec['change'],
+}
