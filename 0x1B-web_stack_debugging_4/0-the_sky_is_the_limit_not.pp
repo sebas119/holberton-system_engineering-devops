c42c3f9f6277b71b92a1b909e27f0c8c3838c1eb
Sebastian Lopez H <lopez.sebastian@correounivalle.edu.co>
2020-05-29 01:35:05 +0000

    Puppet sky is the limit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,7 @@
+# Sky is the limit, let's bring that limit higher
+
 exec { 'change':
-  command  => 'sed -i "s/15/4096/g" /etc/default/nginx',
+  command  => 'sed -i "s/15/2000/g" /etc/default/nginx',
   provider => 'shell',
 }
 
